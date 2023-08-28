41e79652b09648194a951163c05b943fd5c9e272
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-08-28 20:43:10 +0800

    Update Bonezegei_SSD1306.h

--- src/Bonezegei_SSD1306.h
+++ src/Bonezegei_SSD1306.h
@@ -7,19 +7,24 @@
 #define _BONEZEGEI_SSD1306_H_
 #include <Arduino.h>
 #include <Wire.h>
-#define SSD1306_ADDRESS_W 0x78
-#define SSD1306_ADDRESS_R 0x79 
+#define SSD1306_ADDRESS_W 0x3C
+#define SSD1306_CMD_MODE 0x80
+#define SSD1306_DATA_MODE 0x40
 
 class Bonezegei_SSD1306 {
 public:
   Bonezegei_SSD1306();
-
+  Bonezegei_SSD1306(int w, int h);
   void begin();
   void sendCommand(uint8_t cmd);
   void sendData(uint8_t data);
-  
-  unsigned char RAM[1024];
-};
+  void clear();
+  void draw();
+  void drawPixel(uint8_t x, uint8_t y, uint8_t pixel);
 
+  unsigned char RAM[1024];
+  uint16_t width;
+  uint16_t height;
+};
 
 #endif
