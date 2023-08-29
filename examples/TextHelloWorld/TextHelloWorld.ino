466df2e1e377737093c9a0fea65f70dc44602590
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-08-29 08:40:17 +0800

    Create TextHelloWorld.ino

---
+++ examples/TextHelloWorld/TextHelloWorld.ino
@@ -0,0 +1,26 @@
+/*
+  OLEd TextHelloWorld
+  Author: Bonezegei (Jofel Batutay)
+  Date: August 2023 
+*/
+
+//TextHelloWorld
+#include "Bonezegei_SSD1306.h"
+Bonezegei_SSD1306 oled(128, 64);
+
+void setup() {
+  oled.begin();
+  oled.clear();
+  oled.drawText(10, 0, "Hello World", 1, arial_8ptBitmaps, arial_8ptDescriptors);
+  oled.draw();  //update the screen
+}
+
+void loop() {
+  String s = "milis:";
+  s += millis();
+
+  oled.drawFilledRectangle(0, 20, 127, 30, 0);
+  oled.drawText(0, 20, s.c_str(), 1, arial_8ptBitmaps, arial_8ptDescriptors);
+  oled.draw();  //update the screen
+  delay(100);
+}
