c193a484849330462d39c06915b156d21ae4c4fc
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-03-28 20:47:54 +0800

    Update Bonezegei_SSD1306.h

--- src/Bonezegei_SSD1306.h
+++ src/Bonezegei_SSD1306.h
@@ -2,12 +2,13 @@
   This Library is written for OLED Display
   Author: Bonezegei (Jofel Batutay)
   Date: August 2023 
+  Updated: March 2024
 */
 #ifndef _BONEZEGEI_SSD1306_H_
 #define _BONEZEGEI_SSD1306_H_
 #include <Arduino.h>
 #include <Wire.h>
-#include "arial8.h"
+#include "Bonezegei_Font.h"
 
 #define SSD1306_ADDRESS_W 0x3C
 #define SSD1306_CMD_MODE 0x80
@@ -25,12 +26,13 @@ public:
   void drawPixel(uint8_t x, uint8_t y, uint8_t pixel);
 
   void drawFilledRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
-  void drawFilledRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2,uint8_t pixel);
+  void drawFilledRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t pixel);
 
   //draw text Fast skips 0
   void setChar8(uint8_t x, uint8_t y, char ch, int bits, uint8_t color);
   void drawChar(int x, int y, char ch, uint8_t color, const char fd[], const int dsc[95][3]);
   void drawText(int x, int y, const char *str, const char fd[], const int dsc[95][3]);
+  void drawText(int x, int y, const char *str, Bonezegei_Font fnt);
 
   //draw text pixel including 0 value
   void _setChar8(uint8_t x, uint8_t y, char ch, int bits, uint8_t color);
@@ -43,6 +45,11 @@ public:
 
   int xRun;
   int yRun;
+
+  Bonezegei_Font Font_Arial8;
+  Bonezegei_Font Font_Arial10;
+  Bonezegei_Font Font_Arial12;
+  Bonezegei_Font Font_Arial14;
 };
 
 #endif
